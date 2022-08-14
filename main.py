94e0eb6ac395e5a153065cc575ba1e4b80fb5708
jose031199 <luisking531@gmail.com>
2022-08-14 10:42:56 -0700

    Crete method in workers to serach,delete and show employees info

--- main.py
+++ main.py
@@ -2,26 +2,50 @@
 from workers import Workers
 def Menu():
     is_exit = False
+    w1 = Workers()
+    list_workers = w1.Create_List()
     while is_exit is not True:
         try:
             options = int(input("Welcome to Work Company \n" +
                             "1. Add Worker \n" +
                             "2. Show Workers \n" +
                             "3. Find Workers by Name \n" +
-                            "4. Find Workers by Position\n" +
+                            "4. Remove Workers by Name\n" +
                             "5. Exit"
                             " Choose option: "))
 
         # Options for Menu
             if options == 1:
-                Add_Employee()
-                #print("Firs option")
+
+                name = input("Type your name: ").lower()
+                age = int(input("Type your age:"))
+                sex = input("Choose an sex M = male F= female: ").lower()
+                nation = input("Type your country:").lower()
+                position = input('Type your position:').lower()
+                salary = float(input('Type your salary:'))
+                worker = Workers(position, salary, name, age, sex, nation)
+                worker.Add_Worker(list_workers)
+
             elif options == 2:
-                print("Second option choose")
+                if len(list_workers.get("Workers").get("name"))>0:
+                    w1.Show_Employee(list_workers)
+                else:
+                    print("There is not employees")
+
             elif options == 3:
-                print("Third option choose")
+                if len(list_workers.get("Workers").get("name"))>0:
+                    name = input("Name to search: ")
+                    w1.Find_By_Name(name,list_workers)
+                else:
+                    print("There is not employees")
+
             elif options == 4:
-                print("Fourth Options Choose")
+                if len(list_workers.get("Workers").get("name"))>0:
+                    position = input("Position to search: ")
+                    w1.Remove_By_Name(name,list_workers)
+                else:
+                    print("There is not employees")
+
             elif options == 5:
                 print("GoodBye")
                 is_exit = True
@@ -29,15 +53,5 @@ def Menu():
             print(f"{e}")
             Menu()
 
-def Add_Employee():
-    name = input("Type your name: ").lower()
-    age  = int(input("Type your age:"))
-    sex = input("Choose an sex M = male F= female: ").lower()
-    nation = input("Type your country:").lower()
-    position = input('Type your position:').lower()
-    salary = float(input('Type your salary:'))
-    employee = Workers(position,salary,name,age,sex,nation)
-    #print(employee.name)
-    employee.Add_Worker(employee)
 
 Menu()
--- workers.py
+++ workers.py
@@ -1,15 +1,69 @@
 #Class to create Workers
 from person import Person
+import json
 
 class Workers(Person):
-    def __init__(self,position,salary,name,age,sex,nationality):
+    def __init__(self,position='',salary=0,name='',age=0,sex='',nationality=''):
         super().__init__(name,age,sex,nationality)
         self.position = position
         self.salary = salary
-        self.list = {
-            "Worker":{"person":[]}
+
+    def Add_Worker(self,list):
+        list["Workers"].get("name").append(self.name)
+        list["Workers"].get("age").append(self.age)
+        list["Workers"].get("sex").append(self.sex)
+        list["Workers"].get("country").append(self.nationality)
+        list["Workers"].get("position").append(self.position)
+        list["Workers"].get("salary").append(self.salary)
+        print(f'Added new employee {list["Workers"].get("name")[0]}')
+
+    def Show_Employee(self,list):
+        for key,value in list["Workers"].items():
+            print(f"{key}: {value}")
+
+    def Find_By_Name(self,name,list):
+        if name in list["Workers"].get("name"):
+            index = list["Workers"].get("name").index(name)
+            print(f"Name: {list['Workers']['name'][index]}")
+            print(f"Age: {list['Workers']['age'][index]}")
+            print(f"Position: {list['Workers']['position'][index]}")
+            print(f"Country: {list['Workers']['country'][index]}")
+            print(f"Salary: {list['Workers']['salary'][index]}")
+        else:
+            print("No hay ninguna persona con ese nombre")
+
+    def Remove_By_Name(self,name,list):
+        if name in list["Workers"].get("name"):
+            index = list["Workers"].get("name").index(name)
+            list['Workers']['name'].pop(index)
+            list['Workers']['age'].pop(index)
+            list['Workers']['position'].pop(index)
+            list['Workers']['country'].pop(index)
+            list['Workers']['sex'].pop(index)
+            list['Workers']['salary'].pop(index)
+        else:
+            print("No hay ninguna persona con ese nombre")
+
+    def Add_File(self):
+        try:
+            worker_file = open("worker.txt","w")
+            worker_file.write()
+        except Exception as e:
+            print(f"{e} file not created")
+
+
+
+    def Create_List(self):
+        list_workers = {
+            "Workers":{
+                "name":[],
+                "age":[],
+                "sex":[],
+                "country":[],
+                "position":[],
+                "salary":[]
+            }
         }
 
-    def Add_Worker(self,Employee):
-        self.list["Worker"].get("person").append(Employee)
-        print("Added new employee")
+
+        return list_workers
